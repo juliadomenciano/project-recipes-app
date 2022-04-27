7a58f65a80b4edbdfd09ab84a39ced1b782b384c
alisson <alissongod95@hotmail.com>
2022-04-27 16:24:25 -0300

    corrige erros de lint nos testes do login

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,12 +1,26 @@
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
+import DrinksProvider from '../context/DrinksContext/DrinksProvider';
+import FoodsProvider from '../context/FoodsContext/FoodsProvider';
+import ProfileProvider from '../context/ProfileContext/ProfileProvider';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <DrinksProvider>
+        <FoodsProvider>
+          <ProfileProvider>
+            <Router history={ history }>
+              {component}
+            </Router>
+          </ProfileProvider>
+        </FoodsProvider>
+      </DrinksProvider>,
+    ),
+    history,
   });
 };
 export default renderWithRouter;
--- src/tests/testLogin.test.js
+++ src/tests/testLogin.test.js
@@ -1,30 +1,58 @@
-import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const loginSubimitButton = 'login-submit-btn';
+const emailInput = 'email-input';
+const passwordInput = 'password-input';
+const emailInputTest = 'pessoa@email.com';
 
 describe('Testa se página de login renderiza na rota /', () => {
   test('A pagina de login deve possuir dois inputs, email e senha', () => {
     renderWithRouter(<App />);
-    const inputEmail = screen.getByTestId('email-input');
-    const inputPassword = screen.getByTestId('password-input');
-
+    const inputEmail = screen.getByTestId(emailInput);
+    const inputPassword = screen.getByTestId(passwordInput);
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
   });
-  test('A pagina de login deve possuir um botao Enter', () => {
-    renderWithRouter(<App />);
-    const enterButton = screen.getByTestId('login-submit-btn');
-
-    expect(enterButton).toBeInTheDocument();
-  });
   test('Verifica se e possivel escrever no input de email e de senha', () => {
     renderWithRouter(<App />);
-    // const inputEmail = screen.getByTestId('email-input');
-    // const inputPassword = screen.getByTestId('password-input');
-
-    // userEvent.type(inputEmail, 'pessoa@email.com');
-    // userEvent.type(inputPassword, '123456');
+    const inputEmail = screen.getByTestId(emailInput);
+    const inputPassword = screen.getByTestId(passwordInput);
+    userEvent.type(inputEmail, emailInputTest);
+    userEvent.type(inputPassword, '1234567');
+  });
+  test('A pagina de login deve possuir um botao Enter', () => {
+    renderWithRouter(<App />);
+    const enterButton = screen.getByTestId(loginSubimitButton);
+    expect(enterButton).toBeInTheDocument();
+  });
+  test('Ao clicar no botao o usuario deve ser redirecionado para a rota /foods', () => {
+    const { history } = renderWithRouter(<App />);
+    const enterButton = screen.getByTestId(loginSubimitButton);
+    const inputEmail = screen.getByTestId(emailInput);
+    const inputPassword = screen.getByTestId(passwordInput);
+    userEvent.type(inputEmail, emailInputTest);
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(enterButton);
+    expect(history.location.pathname).toBe('/foods');
+  });
+  test(`ao clicar no botao, o email do usuario e os 
+  tokens meal e cocktail devem estar salvos no local storage`, () => {
+    renderWithRouter(<App />);
+    const enterButton = screen.getByTestId(loginSubimitButton);
+    const inputEmail = screen.getByTestId(emailInput);
+    const inputPassword = screen.getByTestId(passwordInput);
+    userEvent.type(inputEmail, emailInputTest);
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(enterButton);
+    const email = localStorage.getItem('user');
+    const meal = localStorage.getItem('mealsToken');
+    const cocktail = localStorage.getItem('cocktailsToken');
+    expect(email).not.toBe(undefined);
+    expect(meal).not.toBe(undefined);
+    expect(cocktail).not.toBe(undefined);
   });
 });
