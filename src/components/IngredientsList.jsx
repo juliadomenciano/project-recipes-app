6b2e827cf434c8ac77e3614cb36de4680af8fdc8
Pedro Tristao <pedroptristao@hotmail.com>
2022-05-02 19:20:22 -0300

    33 ao 38 parciais

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 class IngredientsList extends React.Component {
   render() {
     const { data } = this.props;
     const onlyKeys = Object.keys(data);
+    const mn = 9;
     console.log(onlyKeys);
+    const quant = onlyKeys.filter((key) => (
+      key.match('strMeasure') && data[key] !== ''
+    ));
+
     return (
       <ol>
-        {onlyKeys.map((key) => (
-          key.match('strIngredient') && data[key] !== '' ? <li>{data[key]}</li> : ''
+        {onlyKeys.map((key, index) => (
+          key.match('strIngredient') && data[key] !== '' ? (
+            <div data-testid={ `${index - mn}-ingredient-name-and-measure` }>
+              <li key={ index }>{data[key]}</li>
+              <li>{data[quant[index]]}</li>
+            </div>
+          ) : ''
+
         ))}
       </ol>
     );
